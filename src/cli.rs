89de26d7ef82d60c91e2725d0254e40458651e37
Arijit Basu <sayanarijit@gmail.com>
2026-05-01 07:45:08 +0530

    Edit

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,9 @@
+use crate::dec::decrypt_file;
+use crate::enc::encrypt_file;
 use crate::{
     DecryptOptions, DecryptionMode, DiffOptions, EncryptOptions, EncryptionMode, OperationKind,
-    Project, SyncOptions, clean_project, decrypt_path, diff, encrypt_path, load_identities,
-    load_recipients, status_path, sync_path,
+    Project, SyncOptions, clean_project, decrypt_path, diff, encrypt_path, is_encrypted_path,
+    load_identities, load_recipients, status_path, sync_path, to_decrypted_path, to_encrypted_path,
 };
 use anyhow::{Result, anyhow};
 use clap::Parser;
@@ -11,11 +13,18 @@ use std::path::{Path, PathBuf};
 #[command(author, version, about, long_about = None)]
 struct CottageCli {
     #[command(subcommand)]
-    command: Commands,
+    command: Option<Commands>,
+
+    /// The file to edit
+    path: Option<PathBuf>,
 }
 
 #[derive(clap::Subcommand, Debug)]
 enum Commands {
+    /// Edit a secret
+    #[command(name = "edit", aliases = ["ed"])]
+    Edit(EditArgs),
+
     /// Encrypt secrets
     #[command(name = "encrypt", aliases = ["e", "enc"])]
     Encrypt(EncryptArgs),
@@ -60,6 +69,63 @@ struct CleanArgs {
     /// Suppress all output except errors.
     #[arg(short, long)]
     quiet: bool,
+}
+
+#[derive(clap::Args, Debug, Default)]
+struct EditArgs {
+    /// The file to edit.
+    path: PathBuf,
+
+    /// Encrypt/decrypt with a passphrase.
+    #[arg(short, long)]
+    passphrase: bool,
+
+    /// Encrypt to the specified RECIPIENT. Can be repeated.
+    #[arg(short, long)]
+    recipient: Vec<String>,
+
+    /// Encrypt to recipients listed at PATH. Can be repeated.
+    /// Defaults to recipients in .cottage/recipients if not specified.
+    #[arg(short = 'R', long)]
+    recipients_file: Vec<PathBuf>,
+
+    /// Use the identity file at PATH. Can be repeated.
+    /// Defaults to identity in .cottage/identity if not specified.
+    #[arg(short, long)]
+    identity: Vec<PathBuf>,
+
+    /// Encrypt to a PEM encoded format.
+    #[arg(short, long)]
+    armor: bool,
+
+    /// Skip updating timestamps on encrypted and decrypted files.
+    #[arg(long)]
+    skip_timestamps: bool,
+
+    /// Skip adding encrypted and decrypted files to .gitignore.
+    #[arg(long)]
+    skip_gitignore: bool,
+
+    /// Skip preview generation.
+    #[arg(long)]
+    skip_preview: bool,
+
+    /// Verbose output.
+    #[arg(short, long)]
+    verbose: bool,
+
+    /// Suppress all output except errors.
+    #[arg(short, long)]
+    quiet: bool,
+}
+
+impl EditArgs {
+    fn default_with_path(path: PathBuf) -> Self {
+        Self {
+            path,
+            ..Default::default()
+        }
+    }
 }
 
 #[derive(clap::Args, Debug)]
@@ -455,16 +521,91 @@ fn run_clean_cmd(proj: &Project, args: CleanArgs) -> Result<()> {
     Ok(())
 }
 
+fn run_edit_cmd(proj: &Project, args: EditArgs) -> Result<()> {
+    let path = &args.path;
+    if !path.is_file() {
+        return Err(anyhow!("Not a file: {:?}", path));
+    }
+
+    let is_target_encrypted = is_encrypted_path(path);
+
+    let (decrypted_path, encrypted_path) = if is_target_encrypted {
+        let dec = to_decrypted_path(path).ok_or_else(|| anyhow!("Invalid encrypted path"))?;
+        (dec, path.clone())
+    } else {
+        (path.clone(), to_encrypted_path(path))
+    };
+
+    let passphrase = if args.passphrase {
+        let pass = rpassword::prompt_password("Enter passphrase: ")?;
+        Some(pass)
+    } else {
+        None
+    };
+
+    let identities = load_identities(proj, &args.identity)?;
+
+    if is_target_encrypted {
+        let mode = if let Some(pass) = passphrase.clone() {
+            DecryptionMode::Passphrase(pass)
+        } else {
+            DecryptionMode::Identities(&identities)
+        };
+        let options = DecryptOptions {
+            mode,
+            skip_gitignore: args.skip_gitignore || proj.git().is_none(),
+            skip_timestamps: args.skip_timestamps,
+            skip_checksum_encrypted: false,
+            skip_checksum_decrypted: false,
+        };
+
+        _ = decrypt_file(&encrypted_path, &options)?;
+    }
+
+    edit::edit_file(&decrypted_path)?;
+
+    {
+        let recipients = load_recipients(proj, &args.recipient, &args.recipients_file)?;
+        let mode = if let Some(passphrase) = passphrase {
+            EncryptionMode::Passphrase(passphrase)
+        } else {
+            EncryptionMode::Recipients(&recipients)
+        };
+
+        let options = EncryptOptions {
+            mode,
+            identities: &identities,
+            armor: args.armor,
+            skip_gitignore: args.skip_gitignore || proj.git().is_none(),
+            skip_timestamps: args.skip_timestamps,
+            skip_checksum: false,
+            skip_preview: args.skip_preview,
+        };
+
+        let _ = encrypt_file(&decrypted_path, &options)?;
+    }
+
+    Ok(())
+}
+
 pub fn run() -> Result<()> {
     let cli = CottageCli::parse();
     let proj = Project::init()?;
 
     match cli.command {
-        Commands::Encrypt(args) => run_encrypt_cmd(&proj, args),
-        Commands::Decrypt(args) => run_decrypt_cmd(&proj, args),
-        Commands::Sync(args) => run_sync_cmd(&proj, args),
-        Commands::Status(args) => run_status_cmd(&proj, args),
-        Commands::Diff(args) => run_diff_cmd(&proj, args),
-        Commands::Clean(args) => run_clean_cmd(&proj, args),
+        Some(Commands::Encrypt(args)) => run_encrypt_cmd(&proj, args),
+        Some(Commands::Decrypt(args)) => run_decrypt_cmd(&proj, args),
+        Some(Commands::Sync(args)) => run_sync_cmd(&proj, args),
+        Some(Commands::Status(args)) => run_status_cmd(&proj, args),
+        Some(Commands::Diff(args)) => run_diff_cmd(&proj, args),
+        Some(Commands::Clean(args)) => run_clean_cmd(&proj, args),
+        Some(Commands::Edit(args)) => run_edit_cmd(&proj, args),
+        None => {
+            if let Some(path) = cli.path {
+                run_edit_cmd(&proj, EditArgs::default_with_path(path))
+            } else {
+                Err(anyhow!("No command or path provided"))
+            }
+        }
     }
 }
